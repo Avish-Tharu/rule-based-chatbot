fb437dc7b45257ca36f8a6764c5a2b611fe1e1f1
tharu-nsbm <tawrajapaksha@students.nsbm.ac.lk>
2026-05-08 00:38:24 +0530

    Day 2: Improved chatbot with keyword groups

--- chatbot.py
+++ chatbot.py
@@ -1,14 +1,23 @@
+responses = {
+    "greeting": ["hello", "hi", "hey"],
+    "how_are_you": ["how are you", "how are you doing"],
+    "bye": ["bye", "exit", "quit"]
+}
+
 print("Chatbot started! Type 'bye' to exit.")
 
 while True:
     user_input = input("You: ").lower()
 
-    if "hello" in user_input:
-        print("Bot: Hi there!")
-    elif "how are you" in user_input:
-        print("Bot: I'm fine, thanks!")
-    elif "bye" in user_input:
+    if any(word in user_input for word in responses["greeting"]):
+        print("Bot: Hello! 👋")
+
+    elif any(word in user_input for word in responses["how_are_you"]):
+        print("Bot: I'm doing great! How about you?")
+
+    elif any(word in user_input for word in responses["bye"]):
         print("Bot: Goodbye!")
         break
+
     else:
-        print("Bot: I don't understand that.")
+        print("Bot: Sorry, I don't understand that.")
