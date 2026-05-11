d1804eab1cb0cff50fb9381957c72a4040417153
tharu-nsbm <tawrajapaksha@students.nsbm.ac.lk>
2026-05-11 23:12:23 +0530

    Added chatbot memory and response functions

--- chatbot.py
+++ chatbot.py
@@ -1,22 +1,58 @@
 import json
+import random
 
 with open("responses.json", "r") as file:
     responses = json.load(file)
+
+user_name = ""
+
+greeting_responses = [
+    "Hello!",
+    "Hi there!",
+    "Hey!"
+]
+
+how_are_you_responses = [
+    "I'm doing great!",
+    "I'm fine, thanks!",
+    "Doing well!"
+]
+
+
+def get_response(user_input):
+    global user_name
+
+    if "my name is" in user_input:
+        user_name = user_input.replace("my name is", "").strip()
+        return f"Nice to meet you, {user_name}!"
+
+    elif any(word in user_input for word in responses["greeting"]):
+        return random.choice(greeting_responses)
+
+    elif any(word in user_input for word in responses["how_are_you"]):
+        return random.choice(how_are_you_responses)
+
+    elif any(word in user_input for word in responses["bye"]):
+        return "Goodbye!"
+
+    elif "what is my name" in user_input:
+        if user_name:
+            return f"Your name is {user_name}."
+        else:
+            return "I don't know your name yet."
+
+    else:
+        return "Sorry, I don't understand that."
+
 
 print("Chatbot started! Type 'bye' to exit.")
 
 while True:
     user_input = input("You: ").lower()
 
-    if any(word in user_input for word in responses["greeting"]):
-        print("Bot: Hello! 👋")
+    response = get_response(user_input)
 
-    elif any(word in user_input for word in responses["how_are_you"]):
-        print("Bot: I'm doing great! How about you?")
+    print("Bot:", response)
 
-    elif any(word in user_input for word in responses["bye"]):
-        print("Bot: Goodbye!")
+    if response == "Goodbye!":
         break
-
-    else:
-        print("Bot: Sorry, I don't understand that.")
