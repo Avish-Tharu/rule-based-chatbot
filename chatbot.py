c6c9a3c99722a96f7740e2a760b0ae3e87def38e
tharu-nsbm <tawrajapaksha@students.nsbm.ac.lk>
2026-05-12 22:59:11 +0530

    Added chatbot feature expansion

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 import json
 import random
+from datetime import datetime
 
 with open("responses.json", "r") as file:
     responses = json.load(file)
@@ -18,6 +19,12 @@ how_are_you_responses = [
     "Doing well!"
 ]
 
+jokes = [
+    "Why do programmers prefer dark mode? Because light attracts bugs!",
+    "Why did the computer get cold? Because it forgot to close Windows!",
+    "Why was the Python developer calm? Because they handled exceptions well!"
+]
+
 
 def get_response(user_input):
     global user_name
@@ -26,20 +33,46 @@ def get_response(user_input):
         user_name = user_input.replace("my name is", "").strip()
         return f"Nice to meet you, {user_name}!"
 
+    elif "what is my name" in user_input:
+        if user_name:
+            return f"Your name is {user_name}."
+        else:
+            return "I don't know your name yet."
+
     elif any(word in user_input for word in responses["greeting"]):
         return random.choice(greeting_responses)
 
     elif any(word in user_input for word in responses["how_are_you"]):
         return random.choice(how_are_you_responses)
 
+    elif "joke" in user_input:
+        return random.choice(jokes)
+
+    elif "time" in user_input:
+        current_time = datetime.now().strftime("%H:%M:%S")
+        return f"Current time is {current_time}"
+
+    elif "thank you" in user_input or "thanks" in user_input:
+        return "You're welcome!"
+
+    elif "favorite color" in user_input:
+        return "My favorite color is blue!"
+
+    elif "help" in user_input:
+        return (
+            "You can try:\n"
+            "- hello\n"
+            "- my name is Tharushi\n"
+            "- what is my name\n"
+            "- tell me a joke\n"
+            "- time\n"
+            "- thank you\n"
+            "- favorite color\n"
+            "- bye"
+        )
+
     elif any(word in user_input for word in responses["bye"]):
         return "Goodbye!"
-
-    elif "what is my name" in user_input:
-        if user_name:
-            return f"Your name is {user_name}."
-        else:
-            return "I don't know your name yet."
 
     else:
         return "Sorry, I don't understand that."
